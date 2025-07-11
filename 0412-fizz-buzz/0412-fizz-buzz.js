f47ddebd936300d78827dc7dc3056774b84e7cdf
mrjasil <154539635+mrjasil@users.noreply.github.com>
2025-07-11 09:44:58 +0530

    Time: 1 ms (84.62%), Space: 56.8 MB (79.47%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+   let a=[]
+   for(let i=1 ;i<=n; i++){
+    if(i%3==0&& i%5==0){
+        a.push("FizzBuzz")
+    
+
+    }
+    else if(i%3==0){
+        a.push("Fizz")
+    }
+    else if (i%5==0){
+        a.push("Buzz")
+    }
+    else{
+        a.push(i.toString())
+    }
+   }  
+   return a
+};
+
