4d20aa9e43c6c13773b3b966854ce558b8b759ae
mrjasil <154539635+mrjasil@users.noreply.github.com>
2025-09-03 10:16:12 +0530

    Time: 1 ms (42.27%), Space: 55.8 MB (19.1%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -3,12 +3,14 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let sum=0
-    let result =[]
- for (let i =0; i<nums.length; i++
- ){
-    sum +=nums[i]
-    result.push(sum)
- }
- return result
+    const result =[];
+    let sum =0;
+    for(let i=0; i<nums.length;i++){
+        sum+=nums[i];
+        result.push(sum);
+
+    }
+    return result;
 };
+
+console.log(runningSum([1,2,3,4]));
